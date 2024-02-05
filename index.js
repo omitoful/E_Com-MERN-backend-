caaa55f45dc01f8085e77a7865f4d4d05256a1ca
Kevin <kevin@twjoin.com>
2024-02-05 10:20:27 +0800

    basic setup & image storage engine & create upload endpoint

---
+++ index.js
@@ -0,0 +1,47 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+
+// Database connection with mongodb
+mongoose.connect("mongodb+srv://omitoful:asdfghjkl@cluster0.rfi6cdb.mongodb.net/e-com")
+
+// API creation
+app.get("/", (req, res) => {
+    res.send("Express App is Running.")
+})
+
+// Image storage engine
+const storage = multer.diskStorage({
+    destination: "./upload/images",
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+// Creating upload endpoint for images
+app.use("/images", express.static("upload/images"))
+app.post("/upload", upload.single*("product"), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server Running on Port" + port)
+    } else {
+        console.log("Error : " + error)
+    }
+})
