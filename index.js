01152ef24590c4e832a7e728bcaa6b4ed7baf026
Kevin <kevin@twjoin.com>
2024-02-10 00:12:37 +0800

    setup login function

--- index.js
+++ index.js
@@ -168,6 +168,26 @@ app.post("/signup", async (req, res) => {
     res.json({success:true, token})
 })
 
+// Creating Endpoint for user login 
+app.post("/login", async (req, res) => {
+    let user = await Users.findOne({email: req.body.email});
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data, "secret_ecom");
+            res.json({success:true, token})
+        } else {
+            res.json({success:false, errors: "Wrong Password"});
+        }
+    } else {
+        res.json({success:false, errors: "Wrong Email ID"});
+    }
+})
 
 app.listen(port, (error) => {
     if (!error) {
