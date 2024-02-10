60d8f49194c006cd99db65a9c4bd42bee4e9d0f9
Kevin <kevin@twjoin.com>
2024-02-10 15:39:37 +0800

    cart api setup

--- index.js
+++ index.js
@@ -197,6 +197,52 @@ app.get("/newcollections", async (req, res) => {
     res.send(newCollection);
 })
 
+// Creating Endpoint for popular in women data
+app.get("/popularinwomen", async (req, res) => {
+    let products = await Product.find({category: "women"});
+    let popular_in_women = products.slice(0, 4);
+    console.log("Popular in Women Fetched.")
+    res.send(popular_in_women);
+})
+
+// Creating middleware to fetch user
+const fetchUser = async (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({errors: "Please uathenticate using a valid token"})
+    } else {
+        try {
+            const data = jwt.verify(token, "secret_ecom");
+            req.user = data.user
+            next()
+        } catch (error) {
+            res.status(401).send({errors: "Please uathenticate using a valid token"})
+        }
+    }
+}
+
+// Creating Endpoint for adding products to Cartdata
+app.post("/addtocart", fetchUser, async (req, res) => {
+    let userData = await Users.findOne({_id: req.user.id});
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id: req.user.id}, {cartData: userData.cartData})
+    res.send("Added")
+})
+// Creating Endpoint for removing products from Cartdata
+app.post("/removefromcart", fetchUser, async (req, res) => {
+    let userData = await Users.findOne({_id: req.user.id});
+    if (userData.cartData[req.body.itemId] > 0)
+    userData.cartData[req.body.itemId] -= 1;
+    await Users.findOneAndUpdate({_id: req.user.id}, {cartData: userData.cartData})
+    res.send("Removed")
+})
+// Creating Endpoint to get Cartdata
+app.post("/getcart", fetchUser, async (req, res) => {
+    console.log("Get Cart");
+    let userData = await Users.findOne({_id: req.user.id});
+    res.json(userData.cartData);
+})
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server Running on Port" + port)
